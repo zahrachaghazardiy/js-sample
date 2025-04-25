73ae881bdbaa13dfd73bb1de00fc3b1fde7af492
zahra <zahrachaghazardi81@gmail.com>
2025-04-25 16:29:42 +0330

    add js code

---
+++ jsSample/scriipt.js
@@ -0,0 +1,8 @@
+let uname,pass;
+uname=prompt("please enter your username:");
+pass=prompt("please enter your password:");
+if (uname==="admin" && pass==="123") {
+    alert("welcome");
+} else {
+    alert("wrong username or password");
+}
